a797d213b307ab7b970483d8fba611b00af8b4bd
Shafran Naizer <mshafran13@gmail.com>
2021-12-21 13:15:27 +0400

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,13 @@
 export const environment = {
   production: true,
   api_base_url: 'https://bank-of-seamless.herokuapp.com',
+  firebase:{
+    apiKey: "AIzaSyBtNytBDzxcL1dm7TUZeiFhOW96e_Ad4TM",
+    authDomain: "bankofseamless.firebaseapp.com",
+    projectId: "bankofseamless",
+    storageBucket: "bankofseamless.appspot.com",
+    messagingSenderId: "111544366252",
+    appId: "1:111544366252:web:0edccdc1bbce69b4be5b09",
+    measurementId: "G-7F72N8NP54"
+  }
 };
